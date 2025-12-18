0bac51ee08d5f1fda7cfd0b97440507f0c1b8aa3
DrabantBY <yauheni.basalai@gmail.com>
2025-12-18 14:57:24 +0300

    feat(components): add countries

---
+++ src/app/components/countries/countries.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-countries",
+  imports: [],
+  templateUrl: "./countries.component.html",
+  styleUrl: "./countries.component.scss",
+})
+export class CountriesComponent {}
