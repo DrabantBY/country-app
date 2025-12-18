824279ba1c344f768d742d9d3ef76d389ed242fa
DrabantBY <yauheni.basalai@gmail.com>
2025-12-18 14:58:54 +0300

    feat(routes): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import type { Routes } from "@angular/router";
+import { CountriesComponent } from "./components/countries/countries.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "countries",
+    pathMatch: "full",
+  },
+  {
+    path: "countries",
+    component: CountriesComponent,
+  },
+  {
+    path: "**",
+    loadComponent: () =>
+      import("./components/not-found/not-found.component").then(
+        ({ NotFoundComponent }) => NotFoundComponent,
+      ),
+  },
+];
