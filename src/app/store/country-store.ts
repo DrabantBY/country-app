065b48af7951fdd6749add14e468cfd646e176e2
DrabantBY <yauheni.basalai@gmail.com>
2025-12-26 19:17:47 +0300

    chore(deps): add store devtools

--- src/app/store/country-store.ts
+++ src/app/store/country-store.ts
@@ -1,8 +1,10 @@
 import { signalStore, withHooks } from "@ngrx/signals";
+import { withDevtools } from "@angular-architects/ngrx-toolkit";
 
 import { countrySignalFeature, countryEntityFeature } from "./features";
 
 export const CountryStore = signalStore(
+  withDevtools("store"),
   countrySignalFeature(),
   countryEntityFeature(),
   withHooks({
